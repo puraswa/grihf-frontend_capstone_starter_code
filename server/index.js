92f8dbee02f0c0d4bade717ac7c5c43cbf0776b6
puraswa <amenhermano20@gmail.com>
2024-12-25 21:36:58 -0500

    Decimo Septimo Commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 const connectToMongo = require('./db');
 const app = express();
-
-
-app.set('view engine','ejs')
-app.use(express.static('public'))
-
+const path = require('path');
 const PORT = process.env.PORT || 8181;
 
 
@@ -15,19 +10,23 @@ const PORT = process.env.PORT || 8181;
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
+// Conectar a MongoDB
 connectToMongo();
 
-// Routes
+// Rutas
 app.use('/api/auth', require('./routes/auth'));
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('¡Hola Mundo!');
 });
 
-
-
-  // Start the server
+  // Iniciar el servidor
 app.listen(PORT, () => {
-console.log(`Server is running on port http://localhost:${PORT}`);
+console.log(`El servidor está corriendo en el puerto http://localhost:${PORT}`);
 });
+
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    API_URL: '5500'
+};
+
+export default config;
